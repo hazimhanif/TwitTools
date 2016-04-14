56ddcc4705d0bc58f71b77e926188cadb49eb066
Hazim Hanif <hazimhanif0@gmail.com>
2016-04-14 17:22:33 +0800

    Remove clean function

--- TwitClassify/TwitClassify.py
+++ TwitClassify/TwitClassify.py
@@ -16,11 +16,21 @@ from collections import Counter
 global depth
 global dir_id
 global counter
+global counter_human
+global counter_bot
+global counter_cyborg
+global counter_drop
 global url_link
+global url_profile
 
+url_profile=""
 url_link=""
 depth=0
 counter=0
+counter_human=0
+counter_bot=0
+counter_cyborg=0
+counter_drop=0
 dir_id=""
 
 
@@ -28,29 +38,40 @@ def init_auto():
     global dir_id
     global depth
     global counter
+    global counter_human
+    global counter_bot
+    global counter_cyborg
+    global counter_drop
+    global url_profile
+    
+    dir_count()
     while depth<3:
         dir_depth = 'D:/Twitter/Depth-%d' % depth
         list_dir = os.listdir(dir_depth)
         for doc in list_dir:
             counter +=1
             dir_id = '%s/%s' % (dir_depth,str(doc))
-            
-            skip=clean(doc)
-            if skip == True:
-                continue;
-            
-            res=check_file()
-            if res == True:
-                continue;
-            
+
+            print("------------Classification Counter---------------")
+            print("Human  : ", counter_human)
+            print("Bot    : ", counter_bot)
+            print("Cyborg : ", counter_cyborg)
+            print("Drop   : ", counter_drop)
+            print("------------------------------------------------")
+            print("")
             print("User number: ",counter)
-            print("User id: ",str(doc))
-            print("Depth: ",depth)
+            print("User id    : ",str(doc))
+            print("Depth      : ",depth)
             if(input("Get Info? (y/n) : ")=='y'):
                 os.system('cls' if os.name == 'nt' else 'clear')
                 xtract_user_data()
                 xtract_tweet()
                 check_url()
+                
+                if input("Open URL ? (y/n): ") == "y":
+                    webbrowser.open(url_profile, new=0, autoraise=True)
+
+
                 decision(doc)
                 print()
                 print("----------------Done for user: ",doc)
@@ -68,15 +89,15 @@ def xtract_tweet():
         url_count=0
         c=0
         dir_tweet = '%s/tweets.dump' % dir_id
-        print("----Tweets--------")
+        print("-------------------Tweets-----------------------------")
         jfile = codecs.open(dir_tweet,'rb','utf-8')
         for jdoc in jfile:
             jobj = json.loads(jdoc)
             list_url = jobj['entities']['urls']
-            print("Tweet: "+ jobj['text'])
+            print("Tweet        : "+ jobj['text'])
             print("Source Device: "+ jobj['source'])
             for url in list_url:
-                print("URL: " + url['expanded_url'])
+                print("URL          : " + url['expanded_url'])
             print()
             c+=1
             if c==100:
@@ -91,6 +112,9 @@ def xtract_tweet():
                 url_link = url_link + str(url['expanded_url'])+"\n"
                 url_count+=1
                 
+            if url_count> 490:
+                break
+                
         
         url_link = "%d\n%s" %(url_count,url_link)
         jfile2.close()
@@ -99,26 +123,24 @@ def xtract_tweet():
         print(e, " in extract tweets")
 
 def xtract_user_data():
-
+    global url_profile
     try:
         dir_user_profile = '%s/user_data.dump' % dir_id
         jfile = codecs.open(dir_user_profile,'rb','utf-8')
         for jdoc in jfile:
             jobj = json.loads(jdoc)
             print()
-            print("-----User Info------")
-            print("Name: "+ jobj['name'])
-            print("Screen Name: "+ jobj['screen_name'])
-            print("Created At: " +jobj['created_at'])
-            print("Friends count: "+ str(jobj['friends_count']))
-            print("Followers Count: "+str(jobj['followers_count']))
+            print("-----------------User Info---------------------------")
+            print("Name            : "+ jobj['name'])
+            print("Screen Name     : "+ jobj['screen_name'])
+            print("Created At      : " +jobj['created_at'])
+            print("Friends count   : "+ str(jobj['friends_count']))
+            print("Followers Count : "+str(jobj['followers_count']))
             print("Favourites Count: "+str(jobj['favourites_count']))
-            print("Status Count: "+str(jobj['statuses_count']))
+            print("Status Count    : "+str(jobj['statuses_count']))
             print()
-            url="http://www.twitter.com/%s" % jobj['screen_name']
+            url_profile="http://www.twitter.com/%s" % jobj['screen_name']
             
-            if input("Open URL ? (y/n)") == "y":
-                webbrowser.open(url, new=0, autoraise=True)
     
         jfile.close()
         
@@ -126,6 +148,10 @@ def xtract_user_data():
         print(e, " in extract user data")
     
 def decision(doc):
+    global counter_human
+    global counter_bot
+    global counter_cyborg
+    global counter_drop
     inp = "o"
     while not("h" is inp or "b" is  inp or "c" is inp or "d" is inp) :
         inp = input("Please classify: Bot(b), Cyborg(c), Human(h), Drop(d): ")
@@ -142,6 +168,7 @@ def decision(doc):
             dst_f = "%s/%s" %(dst_dir,file)
             shutil.move(src_f, dst_f)
         os.rmdir(dir_id)
+        counter_bot +=1
 
 
     elif inp=="c":   
@@ -155,7 +182,8 @@ def decision(doc):
             src_f = "%s/%s" %(dir_id,file)
             dst_f = "%s/%s" %(dst_dir,file)
             shutil.move(src_f, dst_f)
-        os.rmdir(dir_id)           
+        os.rmdir(dir_id)
+        counter_cyborg +=1           
 
     elif inp=="h":   
         dir_temp = "%s/human" % dir_id   
@@ -168,7 +196,8 @@ def decision(doc):
             src_f = "%s/%s" %(dir_id,file)
             dst_f = "%s/%s" %(dst_dir,file)
             shutil.move(src_f, dst_f)
-        os.rmdir(dir_id)       
+        os.rmdir(dir_id)
+        counter_human +=1       
 
     else:   
         dir_temp = "%s/drop" % dir_id   
@@ -181,32 +210,8 @@ def decision(doc):
             src_f = "%s/%s" %(dir_id,file)
             dst_f = "%s/%s" %(dst_dir,file)
             shutil.move(src_f, dst_f)
-        os.rmdir(dir_id)   
-
-def clean(doc):
-    list_file = os.listdir(dir_id)
-    if not("followers.txt" in list_file and "friends.txt" in list_file and "tweets.dump" in list_file and "user_data.dump" in list_file):
-        dir_temp = "%s/drop" % dir_id   
-        f = open(dir_temp,"wb")
-        f.close()
-        dst_dir = "D:/Classify/Drop/%s" % doc
-        os.makedirs(dst_dir)
-        for file in list_file:
-            src_f = "%s/%s" %(dir_id,file)
-            dst_f = "%s/%s" %(dst_dir,file)
-            shutil.move(src_f, dst_f)
         os.rmdir(dir_id)
-        return True   
-    else:
-        return False
-
-
-def check_file():
-    list_file = os.listdir(dir_id)
-    if("bot" in list_file or "cyborg" in list_file or "human" in list_file or "drop" in list_file):
-        return True
-    else:
-        return False
+        counter_drop +=1   
 
 def check_url():
     data = url_link
@@ -218,7 +223,19 @@ def check_url():
     for item in wordcount.items():
         print("{}\t{}".format(*item))
     
+def dir_count():
+    global counter
+    global counter_human
+    global counter_bot
+    global counter_cyborg
+    global counter_drop
 
+    counter_bot = len(os.listdir("D:/Classify/Bot/"))
+    counter_human = len(os.listdir("D:/Classify/Human/"))
+    counter_cyborg = len(os.listdir("D:/Classify/Cyborg/"))
+    counter_drop = len(os.listdir("D:/Classify/Drop/"))
+
+    counter = counter_bot + counter_human + counter_cyborg + counter_drop
 
 #Start the extractor
 init_auto()
