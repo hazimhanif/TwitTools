9ac4e35658e279c84a9a1e849cb8be4cced07a2e
Hazim Hanif <hazimhanif0@gmail.com>
2016-04-14 17:31:49 +0800

    TwitClean added.

---
+++ TwitClean/TwitClean.py
@@ -0,0 +1,49 @@
+'''
+Created on Apr 14, 2016
+
+@author: Hazim
+'''
+
+import os
+import shutil
+
+depth=0
+counter=0
+user=0
+
+def clean(doc,dir_id):
+    list_file = os.listdir(dir_id)
+    if not("followers.txt" in list_file and "friends.txt" in list_file and "tweets.dump" in list_file and "user_data.dump" in list_file):
+        dir_temp = "%s/drop" % dir_id   
+        f = open(dir_temp,"wb")
+        f.close()
+        dst_dir = "D:/Classify/Drop/%s" % doc
+        try:
+            os.makedirs(dst_dir)
+        except FileExistsError:
+            shutil.rmtree(dir_id)
+            return
+        
+        list_file = os.listdir(dir_id)
+        for file in list_file:
+            src_f = "%s/%s" %(dir_id,file)
+            dst_f = "%s/%s" %(dst_dir,file)
+            shutil.move(src_f, dst_f)
+        os.rmdir(dir_id)
+        return True   
+    else:
+        return False
+
+while depth<3:
+    dir_depth = 'D:/Twitter/Depth-%d' % depth
+    list_dir = os.listdir(dir_depth)
+    for doc in list_dir:
+        user+=1
+        dir_id = '%s/%s' % (dir_depth,str(doc))
+        skip=clean(doc,dir_id)
+        print(user)
+        if skip==True:
+            counter +=1
+            print(counter)
+    
+    depth +=1
---
+++ TwitClean/__init__.py
@@ -0,0 +1,6 @@
+'''
+Created on Apr 14, 2016
+
+@author: Hazim
+'''
+# Init file for TwitClean
